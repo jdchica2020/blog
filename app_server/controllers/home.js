901b8471a7e57b9cab5790f5d013f578e9b6455a
jdchica2020 <joanchica2003@gmail.com>
2020-09-28 21:29:04 -0400

    lab3

--- app_server/controllers/home.js
+++ app_server/controllers/home.js
@@ -1,6 +1,6 @@
 /* GET homepage */ 
 const home = (req, res, next) => { 
-res.render('home', { title: 'Joan Chica' });
+    res.render('home', { title: 'Joan Chica' });
 };
 module.exports = { 
   home
